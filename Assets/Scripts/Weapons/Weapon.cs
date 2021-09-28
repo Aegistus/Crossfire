af4157bc11f8f3c1f84a2b7e2e9ea526dbbfe2a5
Aegistus <joe.ballanders@gmail.com>
2021-09-27 21:46:19 -0400

    Add BAR Particle Effects and Rework Burst-Fire Particle System

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private WeaponStats stats;
     [SerializeField]
+    private ParticleSystem[] gunSmokeParticles;
+    [SerializeField]
     private ParticleSystem[] shotParticles;
 
     public WeaponStats Stats { get => stats; }
@@ -15,10 +17,28 @@ public class Weapon : MonoBehaviour
     {
         if (shotParticles != null)
         {
+            StartCoroutine(RepeatedShots());
+        }
+        if (gunSmokeParticles != null)
+        {
+            for (int i = 0; i < gunSmokeParticles.Length; i++)
+            {
+                gunSmokeParticles[i].Play(true);
+            }
+        }
+    }
+
+    public IEnumerator RepeatedShots()
+    {
+        int shotsFired = 0;
+        while (shotsFired < stats.shotBursts)
+        {
             for (int i = 0; i < shotParticles.Length; i++)
             {
                 shotParticles[i].Play(true);
             }
+            yield return new WaitForSeconds(stats.shotInterval);
+            shotsFired++;
         }
     }
 }
--- Assets/Scripts/Weapons/WeaponStats.cs
+++ Assets/Scripts/Weapons/WeaponStats.cs
@@ -6,4 +6,8 @@ using UnityEngine;
 public class WeaponStats : ScriptableObject
 {
     public int hitDice = 1;
+
+    [Header("Particle Effects")]
+    public int shotBursts = 1;
+    public float shotInterval = .1f;
 }
