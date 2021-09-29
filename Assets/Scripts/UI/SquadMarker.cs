2879e2d635a5634d9684c2371346dc0f04bef444
Aegistus <joe.ballanders@gmail.com>
2021-09-28 21:13:01 -0400

    Squad Markers Now Disappear When Camera is Close

--- Assets/Scripts/UI/SquadMarker.cs
+++ Assets/Scripts/UI/SquadMarker.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class SquadMarker : MonoBehaviour
 {
+    public float cullDistance = 5f;
+
     private Transform camTransform;
     private Squad squad;
+    private GameObject childIcon;
 
     private void Start()
     {
         camTransform = Camera.main.transform;
         squad = GetComponentInParent<Squad>();
+        childIcon = transform.GetChild(0).gameObject;
     }
 
     private void Update()
     {
+        if (Mathf.Abs(camTransform.position.y - transform.position.y) < cullDistance)
+        {
+            childIcon.SetActive(false);
+        }
+        else if (!childIcon.activeInHierarchy)
+        {
+            childIcon.SetActive(true);
+        }
         transform.position = squad.Movement.Position;
         transform.LookAt(camTransform);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
     }
 }
