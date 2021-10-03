49ae396a6ba779d9a6c714a9f87d5cd3f495654c
Aegistus <joe.ballanders@gmail.com>
2021-10-03 14:33:24 -0400

    Make UIButtons Auto-Subscribe to Button Component Click

--- Assets/Scripts/UI/UIGroupMoveButton.cs
+++ Assets/Scripts/UI/UIGroupMoveButton.cs
@@ -7,7 +7,10 @@ public class UIGroupMoveButton : UIOrderButton
 
     public override void ClickButton()
     {
-
+        if (targetSquad != null)
+        {
+            playerTeam.Orders.AddCommandToQueue(new GroupMoveCommand(targetSquad, playerTeam));
+        }
     }
 
     public override void OnHideUI()
